0d6faa55ca365a55a948000ae0a4a97ceb603c5c
Daniel Holmes <djholmes1256@gmail.com>
2025-04-17 20:33:04 +0100

    Add function to test custom images. Fix preprocess to only normalise image value instead of labels

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 # Imports
+import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
 
 from preprocess import Preprocess
+
+from PIL import Image
+import numpy as np
+
+def test_custom_image(model, image_path):
+    # Load the image
+    img = Image.open(image_path).convert('L')  # Convert to grayscale
+    img = img.resize((28, 28))  # Resize to 28x28 pixels
+
+    # Preprocess the image
+    img_array = np.array(img).astype('float32') / 255.0  # Normalize to [0, 1]
+    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension (1, 28, 28)
+
+    # Make predictions
+    predictions = model.predict(img_array)
+    predicted_class = np.argmax(predictions[0])
+
+    print(f'Predicted class for the custom image: {predicted_class}')
 
 # Functions 
 def display_images(train_images, train_labels, number_of_images=25):
@@ -47,7 +66,6 @@ def display_images(train_images, train_labels, number_of_images=25):
         plt.xlabel(train_labels[i])
     plt.show()
 
-
 def main():
     # hyperparameters = dict([('sape', 4139)])
 
@@ -62,30 +80,48 @@ def main():
 
     preprocessed_data = Preprocess(mnist)
     x_train, x_test, y_train, y_test = preprocessed_data.split_data()
-    x_train, x_test, y_train, y_test = preprocessed_data.normalise_images()
+    x_train, x_test = preprocessed_data.normalise_images()
     
     # Inital dataset investigation
     # display_images(x_train, y_train)
 
     # Setup model layers
     model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10)
+        tf.keras.Input(shape=(28, 28)),  # Define the input shape explicitly
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
     ])
 
     # Compile model with optimiser, loss function and metrics 
-    model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-    
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+    model.compile(optimizer=optimizer,
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                metrics=['accuracy'])
+        
     # Train model
-    model.fit(x_train, y_train, epochs=5)
+    model.fit(x_train, y_train, epochs=5)        
 
     # Evaluate model on test data using metrics
     test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+
     print('\nTest accuracy:', test_acc)
+    print('\nTest loss:', test_loss)
+
+    probability_model = tf.keras.Sequential([model, 
+                                         tf.keras.layers.Softmax()])
     
+    predictions = probability_model.predict(x_test)
+
+    test_image_position = 0
+    predicted_class = np.argmax(predictions[test_image_position])
+    print(f'Predicted class for the first test image:{predicted_class}')
+    print(f'Actual Class:{y_test[test_image_position]}')
+
+    custom_image_path = "7.png"  # Replace with your image path
+    test_custom_image(probability_model, custom_image_path)
+
     pass
 
 if __name__ == "__main__":
--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+
 class Preprocess:
     def __init__ (self, data):
         self.data = data
@@ -11,6 +13,10 @@ class Preprocess:
         return self.x_train, self.x_test, self.y_train, self.y_test
     
     def normalise_images(self):
-        self.x_train, self.x_test, self.y_train, self.y_test = self.x_train / 255.0, self.x_test / 255.0, self.y_train / 255.0, self.y_test / 255.0
-        return self.x_train, self.x_test, self.y_train, self.y_test
+        self.x_train, self.x_test = self.x_train / 255.0, self.x_test / 255.0
+        return self.x_train, self.x_test
     
+    def batch_shuffle(self, shuffle=10000, batch_size=32):
+        train_ds = tf.data.Dataset.from_tensor_slices((self.x_train, self.y_train)).shuffle(shuffle).batch(batch_size)
+        test_ds = tf.data.Dataset.from_tensor_slices((self.x_test, self.y_test)).batch(batch_size)
+        return train_ds, test_ds
